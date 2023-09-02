a970ff74917da6787882091311567d7ab9f8f1e6
siarhei-kuzmin-coursera <llama.blr.coursera@gmail.com>
2023-09-02 14:06:25 -0400

    Added: update an account

--- service/routes.py
+++ service/routes.py
@@ -107,7 +107,24 @@ def get_account(account_id):
 # DELETE AN ACCOUNT
 ######################################################################
 
-# ... place you code here to DELETE an account ...
+@app.route("/accounts/<int:account_id>", methods=["PUT"])
+def update_account(account_id):
+    """
+    Update and account
+    This endpoint will update an Account based the account_id that is requested
+    """
+    
+    app.logger.info("Request to update an Account with id: %s", account_id)
+    
+    account = Account.find(account_id)
+    
+    if not account:
+        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
+
+    account.deserialize(request.get_json())
+    account.update()
+    
+    return account.serialize(), status.HTTP_200_OK
 
 
 ######################################################################
